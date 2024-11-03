67ebc1bcf6e8beb371666253dfeada1c27feca68
wds-dxh <wdsnpshy@163.com>
2024-11-03 13:49:02 +0800

    添加命令行参数

--- main.py
+++ main.py
@@ -2,8 +2,8 @@
 Author: wds-dxh wdsnpshy@163.com
 Date: 2024-01-11 16:00:42
 LastEditors: wds-dxh wdsnpshy@163.com
-LastEditTime: 2024-11-03 11:52:14
-FilePath: /推流/main.py
+LastEditTime: 2024-11-03 13:47:04
+FilePath: /rtsp_http/main.py
 Description: 
 微信: 15310638214 
 邮箱：wdsnpshy@163.com 
@@ -12,17 +12,17 @@ Copyright (c) 2024 by ${wds-dxh}, All Rights Reserved.
 from flask import Flask, render_template, Response
 import cv2
 import threading
+import argparse  # 新增导入
 
 app = Flask(__name__)
 
 # 定义全局变量保存最新帧
 global_frame = None
 
-def update_frame():
+def update_frame(video_url):  # 修改函数签名
     global global_frame
     # 使用OpenCV捕获摄像头
-    url = "rtsp://cloud-5plus.dxh-wds.top:8555/stream"  # 替换为你的视频流地址
-    camera = cv2.VideoCapture(url)
+    camera = cv2.VideoCapture(video_url)
     while True:
         success, frame = camera.read()
         if not success:
@@ -33,8 +33,13 @@ def update_frame():
             ret, buffer = cv2.imencode('.jpg', frame, [cv2.IMWRITE_JPEG_QUALITY, 80])
             global_frame = buffer.tobytes()
 
+# 解析命令行参数
+parser = argparse.ArgumentParser(description='RTSP to HTTP video streaming')
+parser.add_argument('--url', type=str, required=True, help='RTSP video stream URL')
+args = parser.parse_args()
+
 # 在应用启动时启动后台线程
-threading.Thread(target=update_frame, daemon=True).start()
+threading.Thread(target=update_frame, args=(args.url,), daemon=True).start()
 
 def generate_frames():
     while True:
